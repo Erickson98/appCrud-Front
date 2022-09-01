0ab9f7245aed54a2e1f280bc22f37b8f42c494bd
Erickson98 <eagramonte@intellisys.com.do>
2022-09-01 09:01:55 -0400

    update

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import User from "./Components/User/User.html";
 import { confirmedRefreshToken } from "./Ruter/Account";
 import { SocketContext, socket } from "./Components/Entities/socket.io";
 const App = () => {
-  const [autentication, setAutentication] = useState(true);
+  const [autentication, setAutentication] = useState(false);
   const [admin, setAdmin] = useState(false);
   const handleAutentication = (value) => {
     setAutentication(value);
